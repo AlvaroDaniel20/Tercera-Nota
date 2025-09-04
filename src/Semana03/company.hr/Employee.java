8f9f4314f1368d7d95b524af9a74fabe9a4dc1b2
AlvaroQuintero <adquinterop@ufpso.edu.co>
2025-09-03 19:55:19 -0500

    semana 03

--- src/Semana03/company.hr/Employee.java
+++ src/Semana03/company.hr/Employee.java
@@ -7,7 +7,12 @@ public class Employee {
 
 
     public void setSalary(double salary) {
-        this.salary = salary;
+        if (salary > 0) {
+            this.salary = salary;
+        } else {
+            System.out.println("¡El salario no puede ser inferior o igual a 0!");
+        }
+        
     }
 
     public double getSalary() {
