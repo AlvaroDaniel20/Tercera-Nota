a543498f752e81ef37b0fd0d723cf8d0aa3692c8
AlvaroQuintero <adquinterop@ufpso.edu.co>
2025-09-03 19:49:10 -0500

    Semana 03

---
+++ src/Semana03/company.hr/Employee.java
@@ -0,0 +1,17 @@
+
+
+public class Employee {
+    public String name;
+    private double salary;
+    protected String role;
+
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+}
